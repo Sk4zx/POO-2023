dc51b22cb7b0dddbd04a5ab3bdfc459a78dd751e
Renan Costa <renancs.sousa@gmail.com>
2023-08-11 10:43:37 -0300

    atividade1

---
+++ atividade1/verificaNumeroPrimo.java
@@ -0,0 +1,21 @@
+package atividade1;
+
+import java.util.Scanner;
+
+public class verificaNumeroPrimo {
+    
+    public static void main(String[] args) {
+        System.out.println("Digite um numero para verifiar se ele e primo\n");
+        verificarNumeroPrimo(null);
+
+        }
+
+        public static void verificarNumeroPrimo(Integer n){
+        Scanner numero = new Scanner(System.in);
+
+        n = numero.nextInt();
+
+        if
+        }
+    
+}
