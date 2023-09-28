3fefd3111782730a6a97e56a647fb1dda32f1b79
Renan Costa <renancs.sousa@gmail.com>
2023-09-28 17:26:35 -0300

    atividade6

---
+++ atividade6/Moto.java
@@ -0,0 +1,20 @@
+package atividade6;
+
+// Moto.java
+public class Moto extends Veiculo {
+    private boolean temPartidaEletrica;
+
+    public Moto(String marca, String modelo, boolean temPartidaEletrica) {
+        super(marca, modelo);
+        this.temPartidaEletrica = temPartidaEletrica;
+    }
+
+    public boolean temPartidaEletrica() {
+        return temPartidaEletrica;
+    }
+
+    public void ligarPartidaEletrica() {
+        System.out.println("Ligando a partida elétrica da moto.");
+    }
+}
+
---
+++ atividade6/Veiculo.java
@@ -0,0 +1,28 @@
+package atividade6;
+
+// Veiculo.java
+public class Veiculo {
+    private String marca;
+    private String modelo;
+
+    public Veiculo(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void acelerar() {
+        System.out.println("O veículo está acelerando.");
+    }
+
+    public void frear() {
+        System.out.println("O veículo está freando.");
+    }
+}
